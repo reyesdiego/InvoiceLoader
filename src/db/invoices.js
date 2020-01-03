d1c2d51e7a3b1d5eab34ebee1625a256f2b5b786
Diego Reyes <reyesdiego@hotmail.com>
2020-01-02 21:49:25 -0300

    Third Invoice Create Test - TDD

--- src/db/invoices.js
+++ src/db/invoices.js
@@ -1,8 +1,18 @@
+const uuidv4 = require('uuid/v4');
+
+let incrementalInvoiceNumer = 0;
+
+const getInvoiceNumber = () => ++incrementalInvoiceNumer;
+
 class Invoices {
-    constructor() {}
+    constructor() {
+        this.invoices = [];
+    }
 
     create(invoice) {
-        return new Invoice({ ...invoice });
+        const invoiceNew = new Invoice({ ...invoice });
+        this.invoices.push(invoiceNew);
+        return invoiceNew;
     }
 }
 
@@ -10,7 +20,11 @@ module.exports = new Invoices();
 
 class Invoice {
     constructor({ net, tax }) {
-        this.net = net;
+        this._id = uuidv4();
+        this.date = new Date();
+        this.number = getInvoiceNumber();
+        this.net = parseFloat(net.toFixed(2));
         this.tax = tax;
+        this.total = parseFloat((net * (1 + tax / 100)).toFixed(2));
     }
 }
--- src/tests/db.js
+++ src/tests/db.js
@@ -1,17 +1,29 @@
 const expect = require('chai').expect;
 const invoiceDB = require('../db/invoices');
+
 describe('DB - INVOICE', function() {
     it('Dummy Test', function() {
         expect(1).eq(1);
     });
 
     it('CREATE - Should create and Invoice and return it', async function() {
+        const date = new Date();
         const invoice = {
             tax: 21,
             net: 1000
         };
-        const invoiceNew = await invoiceDB.create(invoice);
+        let invoiceNew = await invoiceDB.create(invoice);
+        expect(invoiceNew).has.property('_id');
         expect(invoiceNew).has.property('tax', 21);
         expect(invoiceNew).has.property('net', 1000);
+        expect(invoiceNew).has.property('total', 1210);
+        expect(invoiceNew).has.property('date');
+        expect(invoiceNew.date.getDay()).eq(date.getDay());
+        expect(invoiceNew.date.getMonth()).eq(date.getMonth());
+        expect(invoiceNew.date.getYear()).eq(date.getYear());
+        expect(invoiceNew).has.property('number', 1);
+
+        invoiceNew = await invoiceDB.create(invoice);
+        expect(invoiceNew).has.property('number', 2);
     });
 });
